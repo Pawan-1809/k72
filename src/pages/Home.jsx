93ecbd79af8527a5d5ce372aac20f7425bd5a1fb
Pawan kumar <pawankr16123114@gmail.com>
2025-08-31 00:26:12 +0530

    Almost Structure developed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Video from '../components/home/Video'
 import HomeHeroText from '../components/home/HomeHeroText'
 import HomeBottomText from '../components/home/HomeBottomText'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Home = () => {
+
+
     return (
-        <div>
+        <div className='text-white'>
             <div className='h-screen w-screen fixed'>
                 <Video />
             </div>
-            <div className='h-screen w-screen relative flex flex-col justify-between'>
+            <div className='h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between'>
                 <HomeHeroText />
                 <HomeBottomText />
             </div>
